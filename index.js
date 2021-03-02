fb93e313abe44bf553d0383923f99d402d64cbfe
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-02 11:40:07 +0100

    Added a function to create a new book object

--- index.js
+++ index.js
@@ -32,7 +32,27 @@ function readBooks(req, res) {
   res.send(data);
 }
 
-function createBook(req, res) {}
+function createBook(req, res) {
+  const newBook = {
+    id: uuidv4(),
+    title: req.body.title,
+    author: req.body.author,
+  };
+
+  if (
+    typeof req.body.title === 'undefined' ||
+    typeof req.body.author === 'undefined'
+  ) {
+    res.status(400);
+    res.send('invalid request');
+    return;
+  } else if (data.some((element) => element.id === newBook.id)) {
+    console.log('This book already exists!');
+  } else {
+    data.push(newBook);
+    res.json({ msg: 'New book added to the list', Books: data });
+  }
+}
 
 app.listen('4000', () => {
   console.log('Server started at port 4000');
